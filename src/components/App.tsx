47622693fbc7a9510b690d7bc8c427c5ae0f843e
Sebastian <selfishworm117@gmail.com>
2024-03-03 22:26:19 +0100

    2nd commit

---
+++ src/components/App.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import "../scss/app.scss";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="cards">
+        <div className="card-display"></div>
+
+        <div className="background-cards">
+          <div className="background-card"></div>
+          <div className="background-card"></div>
+          <div className="background-card"></div>
+          <div className="background-card"></div>
+          <div className="background-card"></div>
+          <div className="background-card"></div>
+          <div className="background-card"></div>
+          <div className="background-card"></div>
+          <div className="background-card"></div>
+          <div className="background-card"></div>
+          <div className="background-card"></div>
+          <div className="background-card"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
