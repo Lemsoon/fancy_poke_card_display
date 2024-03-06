006d7ec95b9c31725f3500ec4f62848029a5dcc1
Sebastian <selfishworm117@gmail.com>
2024-03-06 19:10:57 +0100

    background cards are styles as well.

--- src/components/BackgroundCards.tsx
+++ src/components/BackgroundCards.tsx
@@ -15,9 +15,46 @@ export const BackgroundCards: React.FC<BackgroundCardsInfo> = ({ extraPokemonInf
                   <img src="../src/images/pokemon_card_back.png" alt="" />
                 </div>
                 <div className="background-card-front">
-                  <header>
-                    <h1>{pokemon.species.name}</h1>
+                  <header className="card-header">
+                    <p className="pokemon-name">{pokemon.species.name}</p>
+                    <p className="hp-display">
+                      HP <span className="base-hp">{pokemon.stats[0].base_stat}</span>
+                    </p>
                   </header>
+                  <div className="image-container">
+                    <img
+                      src={pokemon.sprites.other.home.front_default}
+                      alt={pokemon.species.name}
+                      className="pokemon-image"
+                    />
+                  </div>
+                  <div className="abilities-type">
+                    <div className="abilities-display">
+                      <p className="abilities-title">Abilities</p>
+                      <ul className="abilities-list">
+                        {pokemon.abilities.map((abilityInfo: { ability: { name: string } }) => {
+                          return (
+                            <li key={abilityInfo.ability.name} className="ability-list-item">
+                              {abilityInfo.ability.name}
+                              <p></p>
+                            </li>
+                          );
+                        })}
+                      </ul>
+                    </div>
+                    <div className="type-display">
+                      <p className="type-title">Type</p>
+                      <ul className="type-list">
+                        {pokemon.types.map((type: { type: { name: string } }, index: number) => {
+                          return (
+                            <li key={index} className="type-list-item">
+                              {type.type.name}
+                            </li>
+                          );
+                        })}
+                      </ul>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
